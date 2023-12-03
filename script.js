763106174a0d1dfa9d22225fd40874510f5cc50c
TEXNO <fbbozorov@gmail.com>
2023-12-03 14:57:41 +0500

    js-if

---
+++ script.js
@@ -0,0 +1,163 @@
+//if-1
+/*let a = 4;
+let b = -9;
+let c = 23;
+if(a<b && a<c) {
+  console.log(a);
+}else if (b<a && b<c) {
+  console.log(b);
+}else {
+  console.log(c);
+} */
+//if-2
+/*let a = 40;
+let b = -20;
+let c = 23;
+if((b <= a && a <= c) || (c <= a && a <= b)) {
+  console.log(a);
+}else if ((a <= b && b <= c) || (c <= b && b <= a)) {
+  console.log(b);
+}else {
+  console.log(c);
+}*/
+//if-3
+/*let a = 10;
+let b = -4;
+if(a>b) {
+  console.log(a);
+}else if (b>a) {
+  console.log(b);
+}else if (a=b) {
+  console.log(0);
+}
+else {
+  console.log('qaytadan urinib kuring');
+}*/
+//if-4
+/*let a = 5;
+let b = -12;
+if(a>0 && b>0) {
+  console.log('I-chorak');
+}else if(a<0 && b>0) {
+  console.log('II-chorak');
+}else if(a<0 && b<0) {
+  console.log('III-chorak');
+}else if(a>0 && b<0){
+  console.log('IV-chorak');
+}else {
+  console.log("koordinata boshi");
+}*/
+//if-5
+/*let x = -5;
+if(x>0) {
+  console.log(2*Math.sin(x));
+}else if(x<=0) {
+  console.log(x-6);
+}else {
+  console.log('qaytadan urinib kuring');
+}*/
+//if-6
+/*let x = 0;
+if(x<-2 || x>2) {
+  console.log(2*x);
+}else {
+  console.log(-3*x);
+}*/
+//if-7
+/*let x = -9;
+if(x<=0) {
+  console.log(-x);
+}else if(0<x && x<2) {
+  console.log(x**2);
+}else if(x>=2) {
+  console.log(4);
+}else {
+  console.log('eror');
+}*/
+//if-8
+/*let a = 3;
+let b = 4;
+let c = 5;
+if((a+b)>c && (a+c)>b && (c+b)>a) {
+  console.log('yes');
+}else {
+  console.log('no');
+}*/
+//if-9
+/*let x = 11;
+if(x<0) {
+  console.log('freezing');
+}else if(x>=0 && x<=10) {
+  console.log('very cold');
+}else if(x>=11 && x<=20) {
+  console.log('cold');
+}else if(x>=21 && x<=30) {
+  console.log('normal');
+}else if(x>=31 && x<=40) {
+  console.log('hot');
+}else if(x>=40 && x<=100) {
+  console.log('very hot');
+}else {
+  console.log('burn');
+}*/
+//if-10
+/*let a;
+a=-3
+if(a%2==0 && a>0) {
+  console.log('musbat juft son');
+}else if(a%2!=0 && a>0) {
+  console.log('musbat toq son');
+}else if(a%2==0 && a<0) {
+  console.log('manfiy juft son');
+}else if(a%2!=0 && a<0) {
+  console.log('manfiy toq son');
+}else {
+  console.log('son o ga teng');
+}*/
+//if-11
+/*let a;
+a=2000;
+if(a%2==0 && a>=10 && a<100) {
+  console.log('2 xonali juft son');
+}else if(a%2!=0 && a>=10 && a<100) {
+  console.log('2 xonali toq son');
+}else if(a%2!=0 && a>=100 && a<1000) {
+  console.log('3 xonali toq son');
+}else if(a%2==0 && a>=100 && a<1000) {
+  console.log('3 xonali juft son');
+}else {
+  console.log('notugri son kiritdingiz');
+}*/
+//if-12
+/*let n;
+n=2;
+if(n<0) {
+  console.log('notugri');
+}
+else if(n>=0) {
+  console.log(n**2);
+}else {
+  console.log('adashdiz');
+}*/
+//if-13
+/*let n;
+n=30
+if(n>=20 && "tanaffus") {
+  console.log(n,true);
+}else if(n<=20 && "tanaffus") {
+  console.log(n,false);
+}else if(n<=20 && "tanaffus emas") {
+  console.log(n,false);
+}else {
+  console.log('bugun dars yuq');
+}*/
+//if-15
+/*let n;
+n=23;
+if(n>=0 && n<10) {
+    console.log('bir xonali son');
+}else if(n>=10 && n<100) {
+    console.log('ikki xonali son');
+}else if(n>=100 && n<1000) {
+    console.log('uch xonali son');
+}else {console.log("to'rt xonali son");}*/
